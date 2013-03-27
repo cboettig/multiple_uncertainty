6f772fb4309bfb24b29a5559551733743a23b738
Carl Boettiger <cboettig@gmail.com>
2013-03-26 17:13:40 -0700

    matlab code implemented, shows matching results to R code. closes #4

--- inst/matlab/testing.m
+++ inst/matlab/testing.m
@@ -1,6 +1,6 @@
 
 f=@(x, h) (0.2 * (x-h) * (1 - (x-h) ./ 100) + (x-h));
-x_grid = [1:5] * 10;
+x_grid = [1:150];
 h_grid = x_grid;
 Tmax = 5;
 sigma_g = 0.3;
@@ -9,5 +9,4 @@ sigma_i = 0.3;
 delta = 0.01;
 [D, V, M, I, P, Ep, F, G, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta);
 
-D(:,1)
 plot(x_grid, x_grid - x_grid(D(:,1)))
