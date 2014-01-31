0b7170386034eeab9a58e72011c2ed26a2d7e3bc
Carl Boettiger <cboettig@gmail.com>
2014-01-31 14:45:59 -0800

    support uncertainty correctly and allow for log-normal noise

--- inst/matlab/testing.m
+++ inst/matlab/testing.m
@@ -1,13 +1,20 @@
 
-f=@(x, h) (0.2 * (x-h) * (1 - (x-h) ./ 100) + (x-h));
-x_grid = [1:150];
-h_grid = x_grid;
+f=@(x, h) max( (x-h) * (1 - (x-h) ./ 100) + (x-h), 0);
+x_grid = [0:5:100];
+h_grid = x_grid; % Must be same dimensions as x_grid, or L91 errors...  
 Tmax = 5;
-sigma_g = 0.3;
+sigma_g = 0.1;
 sigma_m = 0.;
-sigma_i = 0.3;
-delta = 0.01;
-[D, V, M, I, P, Ep, F, G, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta);
+sigma_i = 0.0;
+delta = 0.05;
+%% Noise type
+%pdf = @(p,mu,s) lognpdf(p ./ mu, 0, s);
+pdf = @(p,mu,s) unifpdf(p, mu .* (1 - s), mu .* (1 + s)); 
 
-plot(x_grid, x_grid - x_grid(D(:,1)))
+[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 
+plot(x_grid, x_grid - x_grid(D(:,1)), '.-')
+ylim([0 100])
+xlim([0 100])
+
+saveas(1, "policyfn.png")
