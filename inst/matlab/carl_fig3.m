a6194cd03439572b51b111d76f51fc648a172769
Carl Boettiger <cboettig@gmail.com>
2014-04-09 15:38:40 -0700

    use sethi parameters

--- inst/matlab/carl_fig3.m
+++ inst/matlab/carl_fig3.m
@@ -11,28 +11,28 @@ delta = 0.05;
 %pdf = @(p,mu,s) lognpdf(p ./ mu, 0, s);
 pdf = @(p,mu,s) unifpdf(p, mu .* (1 - s), mu .* (1 + s)); 
 
-sigma_g = 0.01;
-sigma_m = 0.01;
-sigma_i = 0.01;
+sigma_g = 0.1;
+sigma_m = 0.1;
+sigma_i = 0.1;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 small = x_grid - x_grid(D(:,1));
 
-sigma_g = 0.1;
-sigma_m = 0.01;
-sigma_i = 0.01;
+sigma_g = 0.5;
+sigma_m = 0.1;
+sigma_i = 0.1;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 growth = x_grid - x_grid(D(:,1));
 
 
-sigma_g = 0.01;
-sigma_m = 0.1;
-sigma_i = 0.01;
+sigma_g = 0.1;
+sigma_m = 0.5;
+sigma_i = 0.1;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 measure = x_grid - x_grid(D(:,1));
 
-sigma_g = 0.01;
-sigma_m = 0.01;
-sigma_i = 0.1;
+sigma_g = 0.1;
+sigma_m = 0.1;
+sigma_i = 0.5;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 implement = x_grid - x_grid(D(:,1));
 
@@ -50,5 +50,5 @@ xlabel(XL)
 ylabel(YL)
 title('Figure 3 in Sethi')
 legend('All Low','Large Growth','Large Measurement','Large  Implementation')
-
+print -dpng carl_fig3.png
 
