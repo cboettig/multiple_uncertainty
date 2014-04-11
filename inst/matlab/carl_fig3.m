ac84a3623786099e827e47bc1ef0c3c094438080
Carl Boettiger <cboettig@gmail.com>
2014-04-11 12:29:12 -0700

    update fig3 with correct run

--- inst/matlab/carl_fig3.m
+++ inst/matlab/carl_fig3.m
@@ -6,32 +6,32 @@ x_grid = [0:1:150];
 h_grid = x_grid; % Must be same dimensions as x_grid, or L91 errors...  
 
 Tmax = 10;
-delta = 0.05;
+delta = 0.15;
 
 %pdf = @(p,mu,s) lognpdf(p ./ mu, 0, s);
 pdf = @(p,mu,s) unifpdf(p, mu .* (1 - s), mu .* (1 + s)); 
 
-sigma_g = 0.0;
-sigma_m = 0.0;
-sigma_i = 0.0;
+sigma_g = 0.1;
+sigma_m = 0.1;
+sigma_i = 0.1;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 small = x_grid - x_grid(D(:,1));
 
 sigma_g = 0.5;
-sigma_m = 0.0;
-sigma_i = 0.0;
+sigma_m = 0.1;
+sigma_i = 0.1;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 growth = x_grid - x_grid(D(:,1));
 
 
-sigma_g = 0.0;
+sigma_g = 0.1;
 sigma_m = 0.5;
-sigma_i = 0.0;
+sigma_i = 0.1;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 measure = x_grid - x_grid(D(:,1));
 
-sigma_g = 0.0;
-sigma_m = 0.0;
+sigma_g = 0.1;
+sigma_m = 0.1;
 sigma_i = 0.5;
 [D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
 implement = x_grid - x_grid(D(:,1));
