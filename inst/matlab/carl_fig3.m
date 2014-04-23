9010de90eb4f3bbf063d1f0feea2e8803e19cfcb
Carl Boettiger <cboettig@gmail.com>
2014-04-23 13:12:04 -0700

    update for arbitrary grid settings

--- inst/matlab/carl_fig3.m
+++ inst/matlab/carl_fig3.m
@@ -2,11 +2,13 @@ clear;
 
 f=@(x, h) max( (x-h) * (1 - (x-h) ./ 100) + (x-h), 0);
 
-x_grid = [0:1:150];
-h_grid = x_grid; % Must be same dimensions as x_grid, or L91 errors...  
+x_grid =  linspace(0,150,151); 
+h_grid =  linspace(0,150,151);
+y_grid  = linspace(0,150,151);
+q_grid  = linspace(0,150,151);
 
 Tmax = 10;
-delta = 0.15;
+delta = 0.05;
 
 %pdf = @(p,mu,s) lognpdf(p ./ mu, 0, s);
 pdf = @(p,mu,s) unifpdf(p, mu .* (1 - s), mu .* (1 + s)); 
@@ -14,41 +16,41 @@ pdf = @(p,mu,s) unifpdf(p, mu .* (1 - s), mu .* (1 + s));
 sigma_g = 0.1;
 sigma_m = 0.1;
 sigma_i = 0.1;
-[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
-small = x_grid - h_grid(D(:,1));
+[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf, y_grid, q_grid);
+small = y_grid - q_grid(D(:,1));
 
 sigma_g = 0.5;
 sigma_m = 0.1;
 sigma_i = 0.1;
-[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
-growth = x_grid - h_grid(D(:,1));
+[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf, y_grid, q_grid);
+growth = y_grid - q_grid(D(:,1));
 
 
 sigma_g = 0.1;
 sigma_m = 0.5;
 sigma_i = 0.1;
-[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
-measure = x_grid - h_grid(D(:,1));
+[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf, y_grid, q_grid);
+measure = y_grid - q_grid(D(:,1));
 
 sigma_g = 0.1;
 sigma_m = 0.1;
 sigma_i = 0.5;
-[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf);
-implement = x_grid - h_grid(D(:,1));
+[D, V, M, I, P, Ep, F, f_matrix] =  multiple_uncertainty(f, x_grid, h_grid, Tmax, sigma_g, sigma_m, sigma_i, delta, pdf, y_grid, q_grid);
+implement = y_grid - q_grid(D(:,1));
 
 XL='Fish Stock';
 YL = 'Policy Function, H';
 YL2 = 'Value Funciton';
 colorlines={'b','k--','g.-','r.'};
 figure
-plot(x_grid,small,colorlines{1},...
-     x_grid, growth, colorlines{2},...
-     x_grid, measure, colorlines{3},...
-     x_grid, implement, colorlines{4})
+plot(y_grid,small,colorlines{1},...
+     y_grid, growth, colorlines{2},...
+     y_grid, measure, colorlines{3},...
+     y_grid, implement, colorlines{4})
 axis([0 100 0 100])
 xlabel(XL)
 ylabel(YL)
 title('Figure 3 in Sethi')
 legend('Det','Large Growth','Large Measurement','Large  Implementation')
-plot2svg('carl_figure3.svg')
+plot2svg('multiple_uncertainty.svg')
 
