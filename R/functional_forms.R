d813cc4a685dd25f87782a24141da52c028ab09e
cboettig <rstudio@example.com>
2015-10-05 04:09:04 +0000

    compare different values of model pars, but not directly comparable anyway.
    e.g. "r" in models like BH determines carrying capacity; so differences here are often due to differences in total stock abundances rather than purely functional forms.

--- R/functional_forms.R
+++ R/functional_forms.R
@@ -11,9 +11,9 @@ logistic <- function(x, h, r = 1, K = 100){
 
 # Beverton-Holt
 #' @export
-bevertonholt <- function(x, h, A = 2, B = 100){
+bevertonholt <- function(x, h, r = 1, K = 100){
   S <- max(x - h, 0)
-  max( A * S / (1 + S / B), 0)
+  max( (1 + r) * S / (1 + S / K), 0)
 }
 
 
@@ -26,7 +26,7 @@ ricker <- function(x, h, r = 1, K = 100){
 
 # Gompertz
 #' @export
-gompertz <- function(x, h, a = 1, b = 0.01){
+gompertz <- function(x, h, r = 1, K = 100){
   S <- max(x - h, 0)
-  S * exp(a - b * S)
+  S * exp(r - S / K)
 }
